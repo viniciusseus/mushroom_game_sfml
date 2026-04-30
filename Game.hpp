95cfac79f4b91f7b6e5141cce1129bebdd6fd027
Vinicius Seus <viniciusseus@gmail.com>
2026-04-30 16:32:53 -0300

    first commit

---
+++ Game.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "Window.hpp"
+#include "Game.hpp"
+#include <iostream>
+
+class Game {
+	public:
+		Game();
+		~Game();
+		void HandleInput();
+		void Update();
+		void Render();
+
+		sf::Time GetElapsed();
+		void RestartClock();
+
+		sf::RenderWindow GetRendererWindow();
+		Window* GetWindow();
+
+	private:
+		void MoveMushroom();
+		void MoveSprite(EventDetails* l_details);
+		Window m_window;
+
+		sf::Texture m_mushroomTexture;
+		sf::Sprite m_mushroom;
+		sf::Sprite m_sprite;
+		sf::Vector2i m_increment;
+
+		sf::Clock m_clock;
+		sf::Time m_elapsed;
+};
---
+++ Window.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "EventManager.hpp"
+
+class Window {
+	public:
+		Window();
+		Window(const std::string& l_title, const sf::Vector2u& l_size);
+		~Window();
+		void Setup(const std::string l_title, const sf::Vector2u& l_size);
+		void BeginDraw(); // Clear the window.
+		void EndDraw(); // Display the changes.
+		void Update();
+		bool IsDone();
+		bool IsFullscreen();
+		sf::Vector2u GetWindowSize();
+		void Draw(sf::Drawable& l_drawable);
+
+		bool IsFocused();
+		EventManager* GetEventManager();
+		void ToggleFullscreen(EventDetails* l_details);
+		void Close(EventDetails* l_details = nullptr);
+
+		sf::RenderWindow* GetRenderWindow();
+
+	private:
+		void Destroy();
+		void Create();
+		sf::RenderWindow m_window;
+		sf::Vector2u m_windowSize;
+		std::string m_windowTitle;
+		bool m_isDone;
+		bool m_isFullscreen;
+
+		EventManager m_eventManager;
+		bool m_isFocused;
+};
